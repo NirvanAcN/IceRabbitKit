e510ad191410a6889e427dc3321f1fb4fc139e6c
马浩萌 <1652370555@qq.com>
2020-12-23 16:38:24 +0800

    add move to method for NSMutableArray

---
+++ IceRabbitKit/Classes/Foundation/NSArray/NSMutableArray+mhmCommon.h
@@ -0,0 +1,18 @@
+//
+//  NSMutableArray+mhmCommon.h
+//  IceRabbitKit
+//
+//  Created by 马浩萌 on 2020/12/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSMutableArray (mhmCommon)
+
+- (void)moveObjectAtIndex:(NSUInteger)idx1 toObjectAtIndex:(NSUInteger)idx2;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ IceRabbitKit/Classes/Foundation/NSArray/NSMutableArray+mhmCommon.m
@@ -0,0 +1,22 @@
+//
+//  NSMutableArray+mhmCommon.m
+//  IceRabbitKit
+//
+//  Created by 马浩萌 on 2020/12/23.
+//
+
+#import "NSMutableArray+mhmCommon.h"
+
+@implementation NSMutableArray (mhmCommon)
+
+- (void)moveObjectAtIndex:(NSUInteger)idx1 toObjectAtIndex:(NSUInteger)idx2 {
+    if (idx1 > idx2) {
+        [self removeObjectAtIndex:idx1];
+        [self insertObject:tmpObj atIndex:idx2];
+    } else {
+        [self insertObject:tmpObj atIndex:idx2 + 1];
+        [self removeObjectAtIndex:idx1];
+    }
+}
+
+@end
